b0f0e6c53973e21f50942b39b054f5739d58b3b3
NightmareJx <khalidgames35@gmail.com>
2024-11-19 22:23:50 +0100

    update fix io

--- backend/server.js
+++ backend/server.js
@@ -67,7 +67,7 @@ const server = app.listen(process.env.PORT, () => {
 const io = require("socket.io")(server, {
   pingTimout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chatapp-ca25.onrender.com",
   },
 });
 
